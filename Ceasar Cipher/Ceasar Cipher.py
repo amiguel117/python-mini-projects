6a714533cd242b8fd2711fb1503a54169cbcf31a
Angelo Miguel de Lara <amigueldelara@gmail.com>
2025-08-13 19:42:43 +1200

    Create Ceasar Cipher.py

---
+++ Ceasar Cipher/Ceasar Cipher.py
@@ -0,0 +1,18 @@
+from operator import index
+from pydoc import plaintext
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ''
+    for letters in original_text:
+        shifted_letters = alphabet.index(letters) + shift_amount
+        cipher_text += alphabet[shifted_letters]
+    print(f'Here is the encoded result: {cipher_text}')
+
+encrypt(original_text=text, shift_amount=shift)
