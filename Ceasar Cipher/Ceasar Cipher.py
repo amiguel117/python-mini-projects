cd8eb8c621f35ec4d6462b286f1f5b6a038a349e
Angelo Miguel de Lara <amigueldelara@gmail.com>
2025-08-24 19:43:55 +1200

    Update Ceasar Cipher.py

--- Ceasar Cipher/Ceasar Cipher.py
+++ Ceasar Cipher/Ceasar Cipher.py
@@ -1,5 +1,7 @@
 from operator import index
 from pydoc import plaintext
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
